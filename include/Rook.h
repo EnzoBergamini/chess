a10c1252c079103d09c3ab125d6834320e98b97c
EnzoBergamini <115539037+EnzoBergamini@users.noreply.github.com>
2023-04-29 21:11:57 +0200

    add legal move to Rook class

--- include/Rook.h
+++ include/Rook.h
@@ -12,6 +12,7 @@ class Rook : public Piece{
 public:
     Rook(Couleur color,int id, Square pos);
 
+    bool isLegalMove(Square dest);
 
 };
 
--- src/Rook.cpp
+++ src/Rook.cpp
@@ -5,4 +5,12 @@
 #include "../include/Piece.h"
 #include "../include/Rook.h"
 
-Rook::Rook(Couleur color, int id, Square pos) : Piece(color, (color == white) ? "\u265C" : "\u2656", id, pos) {}
+Rook::Rook(Couleur color, int id, Square pos) :
+Piece(color, (color == white) ? "\u265C" : "\u2656", id, pos) {}
+
+bool Rook::isLegalMove(Square dest) {
+    if (dest.getLine() != this->position.getLine() && dest.getColumn() != this->position.getColumn()){
+        return false;
+    }
+    return true;
+}
